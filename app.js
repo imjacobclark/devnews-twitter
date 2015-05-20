2959782d8f2e9054af77c0604ddba23e3299ea04
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-05-20 21:33:52 +0100

    First iteration

---
+++ app.js
@@ -0,0 +1,53 @@
+var http = require('http');
+	Twitter = require('twitter'); 
+
+function DeveloperNewsTwitter(){
+	this.client = new Twitter({
+		consumer_key: process.env.CON_KEY,
+		consumer_secret: process.env.CON_SEC,
+		access_token_key: process.env.ACC_KEY,
+		access_token_secret: process.env.access_token_secretterm
+	});
+
+	this.hashtags = ["javascript", "php", "docker", "coreos", "node", "npm", "unix", "linux"];
+};
+
+DeveloperNewsTwitter.prototype.tweet = function(tweet){
+	this.client.post('statuses/update', {status: tweet},  function(error, tweet, response){
+		if(error) throw error;
+	});
+}
+
+DeveloperNewsTwitter.prototype.getRandomStory = function(){
+	var _this = this; 
+
+	http.get({
+        host: 'api.devnews.today'
+    }, function(response){
+		var body = '';
+		
+		response.on('data', function(d) {
+			body += d;
+		});
+
+		response.on('end', function() {
+			body = JSON.parse(body);
+
+			var randomStory = Math.floor(Math.random() * body.length + 1);
+			var tweet = (randomStory + 1) + ": " + body[randomStory].title + " - " + body[randomStory].url;
+
+			for(var i = 0; i < _this.hashtags.length; i++){
+				if(tweet.indexOf(_this.hashtags[i]) > -1)
+					tweet = tweet.replace(_this.hashtags[i], "#" + _this.hashtags[i]);
+			}
+
+			if(tweet.length > 140)
+				dnt.getRandomStory();
+			else
+				_this.tweet(tweet);
+		});
+	});
+}
+
+dnt = new DeveloperNewsTwitter();
+setInterval(dnt.getRandomStory(), 1000 * 60 * 60);
